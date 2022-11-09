adb7be4b18b070909051669ca226ef64806117e9
Amir Sabagh <amir.sabagh80@gmail.com>
2022-11-09 12:17:12 -0500

    Implement optional symbols functionality to generatePass function

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const charactersWithSymbols = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+const charactersNoSymbols = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+console.log(charactersNoSymbols);
 
 let passElOne = document.getElementById("pass-box1");
 let passElTwo = document.getElementById("pass-box2");
 let password1 = "", password2 = "";
 let sliderEl = document.getElementById("myRange");
+let checkboxEL = document.getElementById("symbolsCheckbox");
 let passLengthEl = document.getElementById("pass-length");
 let passLength = 0;
+let generateButtonEl = document.getElementById("generate-btn");
+let copyBtn1 = document.getElementById("copy-btn-1");
 
 
-function randomCharIndex() {
-    return Math.floor(Math.random() * 91);
+function randomCharIndex(symboldIncluded) {
+    if(symboldIncluded) return Math.floor(Math.random() * 91);
+    else return Math.floor(Math.random() * 62);
 }
 
 function generatePass() {
-    for(let i = 0; i < passLength; i++) {
-        password1 += characters[randomCharIndex()];
-        password2 += characters[randomCharIndex()];
+    if(checkboxEL.checked) {
+        for(let i = 0; i < passLength; i++) {
+            password1 += charactersWithSymbols[randomCharIndex(true)];
+            password2 += charactersWithSymbols[randomCharIndex(true)];
+        }
+    } else {
+        for(let i = 0; i < passLength; i++) {
+            password1 += charactersNoSymbols[randomCharIndex(false)];
+            password2 += charactersNoSymbols[randomCharIndex(false)];
+        }
     }
     passElOne.value = password1;
     passElTwo.value = password2;
@@ -52,3 +66,7 @@ sliderEl.oninput = function() {
     passLengthEl.textContent = this.value;
     passLength = sliderEl.value;
 }
+
+// checkboxEL.onchange = function() {
+//     console.log(checkboxEL.checked);
+// }
